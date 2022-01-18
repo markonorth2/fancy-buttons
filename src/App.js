d9f78b5e93ffd08220684448cbe49bb14ba16052
Mark Chuang <markchuangth@gmail.com>
2022-01-18 08:52:35 -0500

    create the AngryButton component, the CounterButton component, the LightSwitchButton, the TextRepeaterButton component, and imported all 4 of these new components to App,js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
+  <div className={`App`}>
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
     </div>
-  );
 }
 
 export default App;
